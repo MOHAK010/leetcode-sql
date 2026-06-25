889f827530a73ec8e828648e91c0cd8718910cf0
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-06-26 00:19:02 +0530

    Time: 198 ms (85.02%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,15 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    p.product_id,
+   COALESCE(ROUND(
+    CAST(
+        SUM(p.price * u.units) AS numeric
+    ) / SUM(u.units),
+    2) , 
+    0
+) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
