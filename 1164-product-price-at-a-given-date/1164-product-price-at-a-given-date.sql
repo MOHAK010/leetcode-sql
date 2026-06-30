bc1ac5d102b3f2213444e0fb81883844d1bab0bd
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-06-30 06:35:41 +0530

    Time: 475 ms (11.43%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,10 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT product_id , 10 as price
-FROM Products
-GROUP BY product_id
-HAVING MIN(change_date) > '2019-08-16'
-
-UNION
 
 SELECT p1.product_id,
        p1.new_price AS price
@@ -14,4 +8,11 @@ WHERE p1.change_date = (
     FROM Products p2
     WHERE p2.product_id = p1.product_id
       AND p2.change_date <= '2019-08-16'
-);
+)
+
+UNION
+
+SELECT product_id , 10 as price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
