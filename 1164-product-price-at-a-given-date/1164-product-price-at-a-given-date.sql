62afc1381fde83c1bb3bfed178536941505acfde
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-06-30 06:33:57 +0530

    Time: 484 ms (9.87%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,17 @@
+-- Write your PostgreSQL query statement below
+SELECT product_id , 10 as price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
+
+UNION
+
+SELECT p1.product_id,
+       p1.new_price AS price
+FROM Products p1
+WHERE p1.change_date = (
+    SELECT MAX(p2.change_date)
+    FROM Products p2
+    WHERE p2.product_id = p1.product_id
+      AND p2.change_date <= '2019-08-16'
+);
