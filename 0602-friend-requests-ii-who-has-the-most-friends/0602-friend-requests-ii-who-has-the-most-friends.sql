a57311f6fca04f46aa4d09c4e8aa694e4607c7a0
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-07-06 04:27:09 +0530

    Time: 172 ms (94.5%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+-- Write your PostgreSQL query statement below
+SELECT requester_id AS id,
+COUNT(*) AS num
+FROM (
+    SELECT requester_id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id
+    FROM RequestAccepted
+) t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
